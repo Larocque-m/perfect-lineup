ffc90f44a280b7b1cfc5ff3813fac44afe0aecae
Michael <mlarock711@comcast.net>
2021-04-18 22:18:13 -0400

    pseudo code here to know that I am expecting a false outcome

---
+++ index.js
@@ -0,0 +1,11 @@
+// false when don't have too many players from a single team 2 & 3 
+// false when don't have too many players only 9 
+// have the right amount of players and right number of positions 
+// have three OF and a C, P, 1B, 2B, 3B, SS
+// false when too few players 
+// false when salary greater than 4500
+const validateLineup = () => {
+
+}
+
+module.exports = validateLineup
