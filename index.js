73f8e7035821a91570db70fb6e79093f88c992f3
Michael <mlarock711@comcast.net>
2021-04-18 22:41:31 -0400

    Trying to build function with other function.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// false when don't have too many players from a single team 2 & 3 
-// false when don't have too many players only 9 
-// have the right amount of players and right number of positions 
+// check lineup on a team 
+// check lineup in a game
+// check number of player to be 9 in a lineup  
+// Check to have the right amount of players and right number of positions 
 // have three OF and a C, P, 1B, 2B, 3B, SS
 // false when too few players 
 // false when salary greater than 4500
-const validateLineup = () => {
-
+const validateLineup = (lineup) => {
+  return teamNumber && gameNumber && PlayerNumber && SalaryNumber && PositionNumber
 }
 
 module.exports = validateLineup
